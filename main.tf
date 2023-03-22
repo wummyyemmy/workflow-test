d8e5a00022a996c7b7de7fa3d4c78220ba5a8406
wummyyemmy <wummyyemmy@yahoo.com>
2023-03-22 01:52:28 -0500

    s3 bucket

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_vpc" "class30" {
     server_side_encryption_configuration {
       rule {
         apply_server_side_encryption_by_default {
-          kms_master_key_id = aws_kms_key.{var.bucket_key}.arn
+          kms_master_key_id = aws_kms_key."var.bucket_key".arn
           sse_algorithm     = "aws:kms"
         }
       }
